f98e00e09ff47f71bdb22f368babace2731cadc2
Melika Hodzic <melikahodzic@hotmail.com>
2019-08-19 18:19:41 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,585 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+
+   <meta charset="utf-8">
+	<title>Mels</title>
+	<meta name="description" content="">  
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" href="css/base.css">  
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/vendor.css">    
+	<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+	<script src="js/modernizr.js"></script>
+	<script src="js/pace.min.js"></script>
+	<link rel="icon" type="image/png" href="favicon.png">
+
+</head>
+
+<body id="top" data-spy="scroll" data-target=".navbar-fixed-top">
+	<!-- Navigation
+    ==========================================-->
+<nav id="menu" class="navbar navbar-default navbar-fixed-top">
+		<div class="container"> 
+		  <!-- Brand and toggle get grouped for better mobile display -->
+		  <div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+			<a class="navbar-brand page-scroll" href="#page-top"><img src="favicon.png" style="width: 80px; height: 80px; padding-bottom: 10px;"></a></div>
+		  
+		  <!-- Collect the nav links, forms, and other content for toggling -->
+		  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+			  <li><a href="#intro" class="page-scroll">Welcome</a></li>
+			  <li><a href="#about" class="page-scroll">About</a></li>
+			  <li><a href="#resume" class="page-scroll">Experience</a></li>
+			  <li><a href="#gallery" class="page-scroll">Gallery</a></li>
+			  <li><a href="#services" class="page-scroll">Services</a></li>
+			  <li><a href="#contact" class="page-scroll">Contact</a></li>
+			</ul>
+		  </div>
+		  <!-- /.navbar-collapse --> 
+		</div>
+	  </nav>
+
+   <section id="intro">   
+
+   	<div class="intro-overlay"></div>	
+
+   	<div class="intro-content">
+   		<div class="row">
+
+   			<div class="col-twelve">
+				<?php echo
+	   			'<h5>Hello, folks.</h5>
+				   <h1>It is Melika Hodzic!</h1>
+				   <p class="intro-parag">Come in into a cyber world of this ambitious young developer based in Sarajevo. Also, find me on social media below. <i class="fa fa-heart" aria-hidden="true"></i></p>
+
+	   			<p class="intro-position">
+	   				<span>Software Engineering</span>
+	   				<span>BiH</span> 
+	   			</p>'
+	?>
+	   			<a class="button stroke smoothscroll" href="#about" title="">More About Me</a>
+
+	   		</div>  
+   			
+   		</div>   		 		
+   	</div> <!-- /intro-content --> 
+
+   	<ul class="intro-social">        
+         <li><a href="https://www.facebook.com/Meeeeeeeelica"><i class="fa fa-facebook"></i></a></li>
+         <li><a href="https://ba.linkedin.com/in/melika-hodzic-164860158"><i class="fa fa-linkedin-square"></i></a></li>
+         <li><a href="https://github.com/MelikaH"><i class="fa fa-github"></i></a></li>
+         <li><a href="https://www.instagram.com/melicaa_h/"><i class="fa fa-instagram"></i></a></li>
+      </ul> <!-- /intro-social -->      	
+
+   </section> <!-- /intro -->
+
+
+   <!-- about section
+   ================================================== -->
+   <section id="about">  
+
+   	<div class="row section-intro">
+   		<div class="col-twelve">
+
+   			<h5>About</h5>
+   			<h1>Let me introduce myself.</h1>
+
+   			<div class="intro-info">
+
+   				<img src="images/mel1.jpg" alt="Profile Picture">
+
+   				<p class="lead">I am B.Sc. in Software Engineering from Sarajevo, with experience in software development (especially web). I graduated in 2019 at International University of Sarajevo as the first female student of Software Engineering department. Very optimistic and hard working person craving for new challenges.</p>
+   			</div>   			
+
+   		</div>   		
+   	</div> <!-- /section-intro -->
+
+   	<div class="row about-content">
+
+   		<div class="col-six tab-full">
+
+   			<h3>Profile</h3>
+   			<p>As a first child of a computer engineer from Travnik, she was introduced at her very young age to computers and the world inside of it. Besides that wonderful world, she also enjoys her real life with family and friends.
+			   Some of her biggest passions are travelling and making new memories. Biking, skiing, trekking are just some of the joys this young girl has for this wolrd.</p>
+
+   			<ul class="info-list">
+   				<li>
+   					<strong>Fullname:</strong>
+   					<span>Melika Hodzic</span>
+   				</li>
+   				<li>
+   					<strong>Birth Date:</strong>
+   					<span>September 7, 1996</span>
+   				</li>
+   				<li>
+   					<strong>Job:</strong>
+   					<span>Software developer</span>
+   				</li>
+   				<li>
+   					<strong>Email:</strong>
+   					<span>melikahodzic@hotmail.com</span>
+   				</li>
+				   <li>
+   					<strong>Country:</strong>
+   					<span>Bosnia and Herzegovina</span>
+   				</li>
+				   <li>
+   					<strong>Residence:</strong>
+   					<span>Sarajevo</span>
+   				</li>
+
+   			</ul> <!-- /info-list -->
+
+   		</div>
+
+   		<div class="col-six tab-full">
+
+   			<h3>Skills</h3>
+   			<p>My specialities are designing and making interesting, eyecatching presentations in every way. I will take the responsibility of leadership for the whole group and manage tasks efficiently. I fluently speak English and a little bit of German language. </p>
+
+			   <ul class="skill-bars">
+				   <li>
+				   	<div class="progress percent95"><span>95%</span></div>
+				   	<strong>HTML5</strong>
+				   </li>
+				   <li>
+				   	<div class="progress percent95"><span>95%</span></div>
+				   	<strong>CSS3</strong>
+				   </li>
+				   <li>
+					<div class="progress percent70"><span>70%</span></div>
+					<strong>PHP</strong>
+					</li>
+				   <li>
+				   	<div class="progress percent70"><span>70%</span></div>
+				   	<strong>C++</strong>
+				   </li>
+				   <li>
+				   	<div class="progress percent95"><span>95%</span></div>
+				   	<strong>Java</strong>
+				   </li>
+				   <li>
+					<div class="progress percent90"><span>90%</span></div>
+					<strong>JavaScript/TypeScript</strong>
+					</li>
+				   <li>
+				   	<div class="progress percent85"><span>85%</span></div>
+				   	<strong>Angular CLI</strong>
+				   </li>
+				   <li>
+					<div class="progress percent85"><span>85%</span></div>
+					<strong>SQL</strong>
+					</li>
+					<li>
+					<div class="progress percent85"><span>85%</span></div>
+					<strong>ReactJS</strong>
+					</li>
+					<li>
+					<div class="progress percent70"><span>70%</span></div>
+					<strong>Android Studio</strong>
+					</li>
+					<li>
+					<div class="progress percent90"><span>90%</span></div>
+					<strong>Photoshop</strong>
+					</li>
+      
+				</ul> <!-- /skill-bars -->	
+
+   		</div>
+
+   	</div>
+
+   	<div class="row button-section">
+   		<div class="col-twelve">
+   			<a href="#contact" title="Hire Me" class="button stroke smoothscroll">Hire Me</a>
+   			<a href="./images/cv.pdf" title="Download CV" class="button button-primary" download>Download CV</a>
+   		</div>   		
+   	</div>
+
+   </section> <!-- /process-->    
+
+
+   <!-- resume Section
+   ================================================== -->
+	<section id="resume" class="grey-section">
+
+		<div class="row section-intro">
+   		<div class="col-twelve">
+
+   			<h5>Experience</h5>
+   			<h1>More of my credentials.</h1>
+
+   			<p class="lead">Here you can find information about my education and work experience.</p>
+
+   		</div>   		
+   	</div> <!-- /section-intro--> 
+
+   	<div class="row resume-timeline">
+
+   		<div class="col-twelve resume-header">
+
+   			<h2>Work Experience</h2>
+
+   		</div> <!-- /resume-header -->
+
+   		<div class="col-twelve">
+
+   			<div class="timeline-wrap">
+
+   				<div class="timeline-block">
+
+	   				<div class="timeline-ico">
+	   					<i class="fa fa-briefcase"></i>
+	   				</div>
+
+	   				<div class="timeline-header">
+	   					<h3>Full Stack Developer</h3>
+	   					<p>2018</p>
+	   				</div>
+
+	   				<div class="timeline-content">
+	   					<h4>Talentium Foundation</h4>
+	   					<p>Great experience in newely formed company by my former Professor. I was introduced to the Angular CLI, Angular Material, PHP and ChartJS technologies.</p>
+	   				</div>
+
+	   			</div> <!-- /timeline-block -->
+
+   			</div> <!-- /timeline-wrap -->   			
+
+   		</div> <!-- /col-twelve -->
+   		
+   	</div> <!-- /resume-timeline -->
+   	
+   	<div class="row resume-timeline">
+
+   		<div class="col-twelve resume-header">
+
+   			<h2>Education</h2>
+
+   		</div> <!-- /resume-header -->
+
+   		<div class="col-twelve">
+
+   			<div class="timeline-wrap">
+
+   				<div class="timeline-block">
+
+	   				<div class="timeline-ico">
+	   					<i class="fa fa-graduation-cap"></i>
+	   				</div>
+
+	   				<div class="timeline-header">
+	   					<h3>Bachelor Degree</h3>
+	   					<p>July 2019</p>
+	   				</div>
+
+	   				<div class="timeline-content">
+	   					<h4>International University of Sarajevo </h4>
+	   					<p>Bachelor degree in Software Engineering with GPA of 3.50/4.0</p>
+	   				</div>
+
+			   </div> <!-- /timeline-wrap --> 
+			   <div class="timeline-block">
+
+					<div class="timeline-ico">
+						<i class="fa fa-graduation-cap"></i>
+					</div>
+
+					<div class="timeline-header">
+						<h3>Senior Degree</h3>
+						<p>July 2015</p>
+					</div>
+
+					<div class="timeline-content">
+						<h4>Gymnasium Travnik </h4>
+						<p>High School Degree from general-education secondary school with GPA 4.0/4.0</p>
+					</div>
+
+			</div> <!-- /timeline-wrap -->   		  			
+
+   		</div> <!-- /col-twelve -->
+   		
+   	</div> <!-- /resume-timeline -->
+		
+	</section> <!-- /features -->
+
+<!-- Gallery Section -->
+<section id="gallery">
+		
+		<!-- Gallery Section -->
+<div id="portfolio" class="text-center">
+		<div class="container">
+		  <div class="section-title">
+				<div class="row section-intro">
+					<div class="col-twelve">
+						<h5>Gallery</h5>
+					</div>
+				</div>
+		  </div>
+		  <div class="row">
+			<div class="portfolio-items">
+			  
+			  
+			  <div class="col-sm-6 col-md-4 col-lg-4">
+				<div class="portfolio-item">
+				  <div class="hover-bg"> <a href="images/devdays.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+					<div class="hover-text">
+					  <h4>Dev Days by Bit Alliance</h4>
+					</div>
+					<img src="images/devdays.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+				</div>
+			  </div>
+			  
+			  <div class="col-sm-6 col-md-4 col-lg-4">
+				<div class="portfolio-item">
+				  <div class="hover-bg"> <a href="images/certificate.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+					<div class="hover-text">
+					  <h4>Geek Fest 2018</h4>
+					</div>
+					<img src="images/certificate.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+				</div>
+			  </div>
+			  
+			  <div class="col-sm-6 col-md-4 col-lg-4">
+				<div class="portfolio-item">
+				  <div class="hover-bg"> <a href="images/graduation.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+					<div class="hover-text">
+					  <h4>My graduation</h4>
+					</div>
+					<img src="images/graduation.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+				</div>
+			  </div>
+			  <div class="col-sm-6 col-md-4 col-lg-4">
+				<div class="portfolio-item">
+				  <div class="hover-bg"> <a href="images/talentium.png" title="Project Title" data-lightbox-gallery="gallery1">
+					<div class="hover-text">
+					  <h4>While I was a part of Talentium Foundation</h4>
+					</div>
+					<img src="images/talentium.png" class="img-responsive" alt="Project Title"> </a> </div>
+				</div>
+			  </div>
+			  <div class="col-sm-6 col-md-4 col-lg-4">
+				<div class="portfolio-item">
+				  <div class="hover-bg"> <a href="images/vispak.png" title="Project Title" data-lightbox-gallery="gallery1">
+					<div class="hover-text">
+					  <h4>Vispak challenge</h4>
+					</div>
+					<img src="images/vispak.png" class="img-responsive" alt="Project Title"> </a> </div>
+				</div>
+			  </div>
+			  <div class="col-sm-6 col-md-4 col-lg-4">
+				<div class="portfolio-item">
+				  <div class="hover-bg"> <a href="images/geek.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+					<div class="hover-text">
+					  <h4>Geek Fest participants</h4>
+					</div>
+					<img src="images/geek.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+			
+</section>
+	
+	<!-- services Section
+   ================================================== -->
+	<section id="services">
+
+		<div class="overlay"></div>
+
+		<div class="row section-intro">
+   		<div class="col-twelve">
+
+   			<h5>Services</h5>
+   			<h1>What Can I Do For You?</h1>
+
+   			<p class="lead">I have many qualities that I am ready to exploite. You can hire me for some of these jobs:</p>
+
+   		</div>   		
+   	</div> <!-- /section-intro -->
+
+   	<div class="row services-content">
+
+   		<div id="owl-slider" class="owl-carousel services-list">
+
+	      	<div class="service">	
+
+	      		<span class="icon"><i class="icon-earth"></i></span>            
+
+	            <div class="service-content">	
+
+	            	 <h3>Webdesign</h3>
+
+		            <p class="desc">This is my favorite thing to do! I like to combine design on my own, so I rearely use the templates. 
+	         		</p>
+	         		
+	         	</div> 	         	 
+
+				</div> <!-- /service -->
+
+				<div class="service">	
+
+					<span class="icon"><i class="icon-window"></i></span>                          
+
+	            <div class="service-content">	
+
+	            	<h3>Web Development</h3>  
+
+		            <p class="desc">I have the most experience in web development and for this kind of development I used Angular CLI and ReactJS technologies.
+	         		</p>
+
+	            </div>	                          
+
+			   </div> <!-- /service -->
+
+				<div class="service">
+
+					<span class="icon"><i class="icon-toggles"></i></span>	              
+
+	            <div class="service-content">
+
+	            	<h3>UI/UX Design</h3>
+
+		            <p class="desc">In most of the group projects I have had, my role was an UX/UI design. I really enjoy designing user interface and up until now, everyone was satisfied so I belive I am good at it.
+	         		</p> 
+	         		
+	            </div>                
+
+				</div> <!-- /service -->
+
+			   <div class="service">
+
+			   	<span class="icon"><i class="icon-image"></i></span>	            
+
+	            <div class="service-content">
+
+	            	<h3>Graphics Design</h3>
+
+		            <p class="desc">For this kind of designing I used Adobe Illustrator and Photoshop. Both of these tools are really powerful and every time I use them I learn something new. They are inexhaustible sources of possibilities.
+	        			</p> 
+
+	            </div>	               
+
+			   </div> <!-- /service -->
+			   <div class="service">
+
+			   	<span class="icon"><i class="icon-mobile"></i></span>	            
+
+	            <div class="service-content">
+
+	            	<h3>Mobile Development</h3>
+
+		            <p class="desc">I have some experience in this field to, but only with Android OS. Anyways, I would like to know more and for that, I will need a new project :).
+	        			</p> 
+
+	            </div>	               
+
+			   </div> <!-- /service -->
+
+	      </div> <!-- /services-list -->
+   		
+   	</div> <!-- /services-content -->
+		
+	</section> <!-- /services -->	
+
+
+	<!-- stats Section
+   ================================================== -->
+	<section id="stats" class="count-up">
+
+		<div class="row">
+			<div class="col-twelve">
+
+				<div class="block-1-6 block-s-1-3 block-tab-1-2 block-mob-full stats-list">
+
+					<div class="bgrid stat">
+
+						<div class="icon-part">
+							<i class="icon-pencil-ruler"></i>
+						</div>
+
+						<h3 class="stat-count">
+							15
+						</h3>
+
+						<h5 class="stat-title">
+							Projects Completed
+						</h5>
+
+					</div> <!-- /stat -->					
+
+					<div class="bgrid stat">
+
+						<div class="icon-part">
+							<i class="icon-users"></i>
+						</div>
+
+						<h3 class="stat-count">
+							2000
+						</h3>
+
+						<h5 class="stat-title">
+							Connections
+						</h5>
+
+					</div> <!-- /stat -->
+
+					<div class="bgrid stat">
+
+						<div class="icon-part">
+							<i class="icon-badge"></i>
+						</div>
+
+						<h3 class="stat-count">
+							5
+						</h3>
+
+						<h5 class="stat-title">
+							Awards Received
+						</h5>
+
+					</div> <!-- /stat -->									
+
+
+				</div> <!-- /stats-list -->
+
+			</div> <!-- /twelve -->
+		</div> <!-- /row -->
+
+	</section> <!-- /stats -->
+
+	
+   <!-- contact
+   ================================================== -->
+	<?php
+		include "contact.html"
+	?>
+
+   <!-- footer
+   ================================================== -->
+
+   <?php
+	include "footer.html"
+   ?>
+   <div id="preloader"> 
+    	<div id="loader"></div>
+   </div> 
+
+   <!-- Java Script
+   ================================================== --> 
+	<script type="text/javascript" src="js/bootstrap.js"></script> 
+	<script type="text/javascript" src="js/SmoothScroll.js"></script> 
+	<script type="text/javascript" src="js/nivo-lightbox.js"></script> 
+	<script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+   <script src="js/jquery-2.1.3.min.js"></script>
+   <script src="js/plugins.js"></script>
+   <script src="js/main.js"></script>
+
+</body>
+
+</html>
